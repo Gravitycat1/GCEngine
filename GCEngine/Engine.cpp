839d18ba0ce391307a82db4a2599ea31df6bbe25
Gravitycat1 <gravitycat0@gmail.com>
2018-06-17 22:57:02 -0400

    finished week 2

--- GCEngine/Engine.cpp
+++ GCEngine/Engine.cpp
@@ -2,6 +2,12 @@
 #include "Logger.h"
 
 #include "SDL\SDL.h"
+
+engine::Engine::~Engine()
+{
+	closeSDL();
+}
+
 int engine::Engine::init()
 {
 	
@@ -11,9 +17,46 @@ int engine::Engine::init()
 
 #endif
 
+	initSDL();
+
 	return 0;
 }
 
 void engine::Engine::run()
 {
+}
+
+void engine::Engine::initSDL()
+{
+
+	// assert() <- us this instead
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		Logger::Log("Failed to init SDL", ELogType::LT_Error);
+
+		//return;
+	}
+	else 
+	{
+		m_window = SDL_CreateWindow("GCEngine", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+		if (!m_window)
+		{
+			Logger::Log("Failed to create window", ELogType::LT_Error);
+		}
+		else
+		{
+			m_surface = SDL_GetWindowSurface(m_window);
+		}
+	}
+}
+
+void engine::Engine::closeSDL()
+{
+	SDL_FreeSurface(m_surface);
+	m_surface = nullptr;
+
+	SDL_DestroyWindow(m_window);
+	m_window = nullptr;
+
+	SDL_Quit();
 }
--- GCEngine/Engine.h
+++ GCEngine/Engine.h
@@ -2,15 +2,27 @@
 #ifndef _ENGINE_H
 #define _ENGINE_H
 
-
+struct SDL_Window;
+struct SDL_Surface;
 
 namespace engine
 {
 	class Engine
 	{
+		//window we'll be rendering to
+		 SDL_Window* m_window;
+		 SDL_Surface* m_surface;
+
+
+
 	public:
+		~Engine ();
 		int init();
 		void run();
+
+	private:
+		void initSDL();
+		void closeSDL();
 	};
 
 }
